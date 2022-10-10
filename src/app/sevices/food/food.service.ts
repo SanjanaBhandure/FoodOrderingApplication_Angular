e1ba70928d400c86370aca76d1bd0a2369152ffa
SanjanaBhandure <sweetybhandure@gmail.com>
2022-10-10 13:45:48 +0530

    Added Images of Food To Home Page

---
+++ src/app/sevices/food/food.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+
+  getAll():string[] {
+    return[
+      '/assets/food1.jpg',
+      '/assets/food2.jpg',
+      '/assets/food3.jpg',
+      '/assets/food4.jpg',
+      '/assets/food5.jpg',
+      '/assets/food6.jpg',
+      '/assets/food7.jpg',
+      '/assets/food8.jpg',
+      '/assets/food9.jpg',
+      '/assets/food10.jpg',
+      '/assets/food11.jpg',
+      '/assets/food12.jpg'
+    ]
+  }
+}
